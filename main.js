83e7be8ac1ea547122693cd97489fa50012f9240
Gabrielli Oliveira <gabrielli.osc@gmail.com>
2024-09-19 11:39:42 -0300

    fix: Update response

--- main.js
+++ main.js
@@ -3,21 +3,25 @@ const estagioContainer = document.getElementById('container')
 async function getDefesaCivilAlert() {
 
     try {
-    const apiUrl = import.meta.env.VITE_API_URL
-    const response = await fetch(apiUrl)
+        const apiUrl = import.meta.env.VITE_API_URL
+        const response = await fetch(apiUrl)
+        const data = await response.json()
 
-    const len = response.length
-    const currentAlert = response[len - 1].fields
-    const date = new Date(currentAlert.data_inicial)
+        const currentAlert = data['features'][0].attribute?.risco
+        const date = new Date(data['features'][0].attribute?.inicial_risco)
 
-    const estagioClass = {
-        'Baixo': 'lowAlert',
-        'Médio': 'mediumAlert',
-        'Alto': 'highAlert',
-        'Muito Alto': 'higherAlert'
-    }
-    estagioContainer.classList.add(estagioClass[currentAlert.grav])
-    estagioContainer.innerHTML = `<h2><em>Risco de fogo em vegetação:</em></h2> <h1>${currentAlert.grav}</h1><p>Atualizado em ${date.toLocaleDateString()}</p>`
+        const estagioClass = {
+            'Baixo': 'lowAlert',
+            'Médio': 'mediumAlert',
+            'Alto': 'highAlert',
+            'Muito Alto': 'higherAlert'
+        }
+        if (currentAlert) {
+            estagioContainer.classList.add(estagioClass[currentAlert.grav])
+            estagioContainer.innerHTML = `<h2><em>Risco de fogo em vegetação:</em></h2> <h1>${currentAlert.grav}</h1><p>Atualizado em ${date.toLocaleDateString()}</p>`
+        } else {
+            estagioContainer.innerHTML = '<p>Não foi possível obter o estágio atual</p>'
+        }
     } catch (error) {
         console.error('Erro ao buscar dados da API', error)
         estagioContainer.innerHTML = '<p>Erro ao buscar dados da API</p>'
